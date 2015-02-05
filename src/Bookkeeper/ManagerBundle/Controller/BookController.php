efe412876aea95fba3123a0e72bd18925255fb4c
Giustino Borzacchiello <giustinob@gmail.com>
2015-02-05 19:42:14 +0100

    Added managerbundle views

--- src/Bookkeeper/ManagerBundle/Controller/BookController.php
+++ src/Bookkeeper/ManagerBundle/Controller/BookController.php
@@ -9,17 +9,17 @@ class BookController extends Controller
 {
     public function indexAction()
     {
-
+        return $this->render('BookkeeperManagerBundle:Book:index.html.twig');
     }
 
     public function showAction( $id )
     {
-
+        return $this->render('BookkeeperManagerBundle:Book:show.html.twig');
     }
 
     public function newAction()
     {
-
+        return $this->render('BookkeeperManagerBundle:Book:new.html.twig');
     }
 
     public function createAction( Request $request )
@@ -29,7 +29,7 @@ class BookController extends Controller
 
     public function editAction( $id )
     {
-
+        return $this->render('BookkeeperManagerBundle:Book:edit.html.twig');
     }
 
     public function updateAction( Request $request, $id )
